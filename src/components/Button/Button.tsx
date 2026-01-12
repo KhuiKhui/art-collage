62f2aca705b0a3e362cee87fce74734a49f496ee
KhuiKhui <khoikhuat0807@gmail.com>
2026-01-12 16:19:34 +0800

    feat: create button
    scaffold project
    
    delete default svgs

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,22 @@
+import cn from '@/utils/cn';
+
+interface ButtonProps extends React.ComponentPropsWithRef<'button'> {
+  text: string;
+}
+
+function Button({ text, className, ...base }: ButtonProps) {
+  return (
+    <button
+      className={cn(
+        'bg-tert-accent border-sec-accent rounded-md border-4 p-4 font-bold text-black transition-all',
+        'hover:bg-sec active:border-sec-accent hover:border-transparent active:scale-110',
+        className,
+      )}
+      {...base}
+    >
+      {text}
+    </button>
+  );
+}
+
+export default Button;
---
+++ src/components/Collage/Collage.tsx
@@ -0,0 +1,5 @@
+function Collage() {
+  return <div></div>;
+}
+
+export default Collage;
